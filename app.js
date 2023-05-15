250bb2ebb4e499962ed28410a795c0025e4971f0
laraibfatima12 <128951988+laraibfatima12@users.noreply.github.com>
2023-05-15 10:27:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+//================== question 1 ====================//
+
+
+// let age = 18;
+//alert ("i am" + age + " year old");
+
+
+
+//=================== question 2 =====================//
+
+//let number -of-visits=0;
+// let i=0;
+// for(i=0; i< 1000; i++){
+// number- of-visits= number-of-visits;
+// alert("you have visited + "number-of-visits" +"time")
+//}
+
+//function visits(number-of-visits){
+//retun number-of-visits;
+//}
+
+//================== question 3 =====================//
+
+//let birth-year=2004;
+//document.write 
